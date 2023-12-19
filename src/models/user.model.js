e71aa02cedbdc0de68ab24a38a522e339429be28
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-12-19 21:20:59 +0530

    user model created

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,41 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose,{Mongoose, Schema} from "mongoose";
 
-const userSchems=Schema({
+const userSchems=new Schema({
+             fullName:{
+                type:String,
+                required:true,
+                trim:true,
+                index:true
+             },
+             phone:{
+                 type:Number,
+                 required:true,
+             },
+             email:{
+                type:String,
+                lowercase:true,
+                trim:true,
+             },
+             visitHistory:[
+                {
+                  type:Schema.Types.ObjectId,
+                  ref:"Car" 
+                }
+            ],
+             password:{
+                type:String,
+                required:[true , "Password is required"],
+             },
+             refreshToken:{
+                type:String
+             }
+},
 
-});
+{
+    timestamps:true 
+}
+
+);
 
 const User=mongoose.model("User",userSchems);
-export default userSchems;
+export {User};
