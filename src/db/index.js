24118f64c11b5a0576806a092ea66297c0fb2fc9
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2024-04-01 15:03:26 +0530

    sample env added

--- src/db/index.js
+++ src/db/index.js
@@ -2,16 +2,12 @@ import mongoose, { mongo } from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connect_DB=async()=>{
-      try {
-          
+      try {    
             const connectioninstance=await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
             console.log("Connected Successfully");
-
-      } catch (error) {
-          
+      } catch (error) {  
             console.error("Database can't connect successfully",error);
             process.exit(1);
-
       }
 }
 
